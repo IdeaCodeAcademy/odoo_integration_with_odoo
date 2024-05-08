32a96fb2fac02b49b289cf545f2581211d3530f2
Agga <agga@Aggas-MacBook-Air.local>
2024-05-08 16:55:33 +0630

    [add] api -testing from odoo

---
+++ addons/api_testing/controllers/auth_controller.py
@@ -0,0 +1,16 @@
+from odoo import http
+from odoo.http import request
+
+
+class AuthController(http.Controller):
+    @http.route('/api/login', type='json', auth='user', cors="*", csrf=False)
+    def api_login(self, *args, **kwargs):
+        login = kwargs.get('login')
+        password = kwargs.get('password')
+        uid = http.request.session.authenticate(http.request.session.db, login, password)
+        return {"message": "Login Successfully.", "uid": uid}
+
+    @http.route('/api/get-headers', type='json', auth='user', cors="*", csrf=False)
+    def api_info(self, *args, **kwargs):
+        headers = request.httprequest.headers
+        return headers
